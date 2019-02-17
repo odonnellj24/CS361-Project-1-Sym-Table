9987b55f66d0a2eeeba66f8c6ac83ff9fbc56202
Jack O'Donnell <35511909+odonnellj24@users.noreply.github.com>
2019-02-17 08:21:15 -0600

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+char c = '\n';
+printf("jodonn6%c",c);
+return 0;
+}
+
+void all(){
+
+}
+char your[4];
+
+void cs361(){
+}
+char are='a';
+char belong[4];
+int to =0;;
+char us[37];
